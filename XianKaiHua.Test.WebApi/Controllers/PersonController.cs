f15311fcbcdde3e1b2c2e27b1bb66fa2dc1c5cb3
xiankaihua <2970174267@qq.com>
2021-12-08 11:15:19 +0800

    添加项目文件。

--- XianKaiHua.Test.WebApi/Controllers/PersonController.cs
+++ XianKaiHua.Test.WebApi/Controllers/PersonController.cs
@@ -18,14 +18,24 @@ namespace XianKaiHua.Test.WebApi.Controllers
             this._ipersonServices = personServices_w;
         }
 
-
-        [HttpGet("BlogNews")]
+        [HttpGet("GetBlogNews")]
         public async Task<HostModel> GetBlogNews()
         {
             var id = this.User.FindFirst("Id").Value;
             var data = await _ipersonServices.Query(c => c.tid == id);
             return new HostModel { code = 200, data = data, result = ""};
         }
+
+        [HttpGet("Add")]
+        public async Task<HostModel> Add()
+        {
+            var id = this.User.FindFirst("Id").Value;
+            var data = await _ipersonServices.Add(new Models.Entity.Person {  tid = Guid .NewGuid().ToString()});
+            if(data > 0)
+                return new HostModel { code = 200, data = data, result = "操作成功" };
+            return new HostModel { code = 404, data = data, result = "操作异常" };
+        }
+
         public class HostModel
         {
             public int code { get; set; }
